3f6a284f840adbfe1017a201f7b9b7a8b6b3ae3c
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-31 22:12:36 +0530

    User App Initialized And Also Registered At Project Level

--- shrinktrack/shrinktrack/settings.py
+++ shrinktrack/shrinktrack/settings.py
@@ -14,7 +14,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'user'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
