b4b061103dd74a826c3aaf77f3466f4e395f77a3
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-31 22:37:16 +0530

    Decoraters Added To User Endpoints

--- shrinktrack/user/views.py
+++ shrinktrack/user/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+@api_view(['POST'])
 def create_user(request):
     pass
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_user(request):
     pass
