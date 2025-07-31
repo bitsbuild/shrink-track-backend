3425214b2518a4a1bc173dcf46e91ad6556ab19d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-01 00:34:21 +0530

    Shrinked URL Creation Structure Defined For The Function

--- shrinktrack/api/views.py
+++ shrinktrack/api/views.py
@@ -1,6 +1,28 @@
 from api.models import ShrinkInstanceModel
 from api.serializers import ShrinkInstanceSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
 class ShrinkInstanceViewset(ModelViewSet):
     queryset = ShrinkInstanceModel.objects.all()
     serializer_class = ShrinkInstanceSerializer
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            obj = ShrinkInstanceModel.objects.get(original_url=request.data['original_url'])
+            obj.shrinked_url = None
+            obj.save()
+            return Response(
+                {
+                    "Status":"URL Shrinked Successfuly"
+                },status=HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "Status":"Shrink Process Failed",
+                    "Error":str(e)
+                },status=HTTP_400_BAD_REQUEST
+            )
