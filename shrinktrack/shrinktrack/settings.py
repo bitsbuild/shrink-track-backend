14e26317205ee450b04a4552388b16a6769acedc
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-31 23:04:13 +0530

    API App Initialized And Registered At Project Level

--- shrinktrack/shrinktrack/settings.py
+++ shrinktrack/shrinktrack/settings.py
@@ -15,7 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'user'
+    'user',
+    'api'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- shrinktrack/shrinktrack/urls.py
+++ shrinktrack/shrinktrack/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include('user.urls'))
+    path('user/',include('user.urls')),
+    path('api/',include('api.urls'))
 ]
