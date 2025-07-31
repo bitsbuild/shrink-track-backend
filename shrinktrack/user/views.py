fc5e610662dc3fe995ce64fc9cf0e510b53fe7ab
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-31 22:46:59 +0530

    User App Views Completed

--- shrinktrack/user/views.py
+++ shrinktrack/user/views.py
@@ -1,9 +1,40 @@
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from user.serializer import UserSerializer
 @api_view(['POST'])
 def create_user(request):
-    pass
+    try:
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {
+                "Status":"Account Created Successfully"
+            }
+        )
+    except Exception as e:
+        return Response(
+            {
+                "Status":"Error",
+                "Error":str(e)
+            },status=HTTP_400_BAD_REQUEST
+        )
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
-    pass
+    try:
+        request.user.delete()
+        return Response(
+            {
+                "Status":"Account Deleted Successfully"
+            },status=HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            {
+                "Status":"Error",
+                "Error":str(e)
+            },status=HTTP_400_BAD_REQUEST
+        )
