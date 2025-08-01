cb2184c19fd72200ebb6fff3848eecc97a880643
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-01 23:27:03 +0530

    Redirect Logic Written

--- shrinktrack/shrinktrack/urls.py
+++ shrinktrack/shrinktrack/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from shrinktrack.redirect import redirect_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls')),
-    path('api/',include('api.urls'))
+    path('api/',include('api.urls')),
+    path('<str:code>/',redirect_view,name='redirect')
 ]
