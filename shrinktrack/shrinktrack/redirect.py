43956769367629fc5095fbf4c9ddcdd768cc2dd2
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-02 00:00:59 +0530

    Configured For Render Usage

--- shrinktrack/shrinktrack/redirect.py
+++ shrinktrack/shrinktrack/redirect.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404,redirect
 from api.models import ShrinkInstanceModel
 def redirect_view(request,code):
-    instance = get_object_or_404(ShrinkInstanceModel,shrinked_url=f'http://localhost:8000/{code}/')
+    instance = get_object_or_404(ShrinkInstanceModel,shrinked_url=f'https://shrink-track.onrender.com/{code}/')
     return redirect(instance.original_url)
--- shrinktrack/shrinktrack/settings.py
+++ shrinktrack/shrinktrack/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('DSK')
 DEBUG = True
-ALLOWED_HOSTS = ['shrink-track.onrender.com']
+ALLOWED_HOSTS = ['shrink-track.onrender.com','127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
